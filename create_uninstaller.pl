06f84a4c10b559a5fd8e57ea3c092e8bb2338cb0
rovo89 <android@robv.de>
2015-06-04 16:01:17 +0200

    Add a script to generate uninstaller ZIP archives
    These ZIP archives contain the special BusyBox executable and a script that
    reverts all the steps performed by the installation script.

---
+++ create_uninstaller.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin qw($Bin);
+use lib $Bin;
+
+use Xposed;
+
+use Archive::Zip qw(:ERROR_CODES :CONSTANTS);
+use Data::Dumper;
+use File::Basename;
+use File::Copy;
+use File::Path qw(make_path remove_tree);
+use Getopt::Std;
+use POSIX qw(strftime);
+use Tie::IxHash;
+use Term::ANSIColor;
+
+our $VERSION = '1.0';
+
+my %opts;
+$| = 1;
+
+# Main routine
+sub main() {
+    $Getopt::Std::STANDARD_HELP_VERSION = 1;
+    getopts('') || exit 2;
+
+    # Load the config file
+    print_status("Loading config file $Bin/build.conf...", 0);
+    $Xposed::cfg = Xposed::load_config("$Bin/build.conf") || exit 1;
+
+    # Check some build requirements
+    print_status('Checking requirements...', 0);
+    Xposed::check_requirements() || exit 1;
+
+    print_status('Creating ZIP archives...', 0);
+    foreach my $platform ('arm', 'x86', 'armv5') {
+        create_zip($platform) if -d "$Bin/zipstatic/$platform" || exit 1;
+    }
+
+    print_status('Done!', 0);
+}
+
+sub create_zip() {
+    my $platform = shift;
+
+    # Create a new ZIP file
+    my $zip = Archive::Zip->new();
+    $zip->addTree($Bin . '/zipstatic/_uninstaller', '') == AZ_OK || return 0;
+    $zip->addTree($Bin . '/zipstatic/' . $platform, '') == AZ_OK || return 0;
+
+    # Set last modification time to "now"
+    my $now = time();
+    foreach my $member($zip->members()) {
+        $member->setLastModFileDateTimeFromUnix($now);
+    }
+
+    # Write the ZIP file to disk
+    my $outdir = $Xposed::cfg->val('General', 'outdir');
+    my $zipname = sprintf('%s/uninstaller/xposed-uninstaller-%s.zip', $outdir, $platform);
+    make_path(dirname($zipname));
+
+    print "$zipname\n";
+    $zip->writeToFileNamed($zipname) == AZ_OK || return 0;
+
+    # TODO: Sign the resulting zip without breaking some "unzip" in some recoveries
+}
+
+sub HELP_MESSAGE() {
+}
+
+sub VERSION_MESSAGE() {
+    print "Xposed uninstaller creation script, version $VERSION\n";
+}
+
+main();
---
+++ zipstatic/_uninstaller/META-INF/com/google/android/flash-script.sh
@@ -0,0 +1,116 @@
+##########################################################################################
+#
+# Xposed framework uninstaller zip.
+#
+# This script removes the Xposed framework files from the system partition.
+# It doesn't touch the Xposed Installer app.
+#
+##########################################################################################
+
+grep_prop() {
+  REGEX="s/^$1=//p"
+  shift
+  FILES=$@
+  if [ -z "$FILES" ]; then
+    FILES='/system/build.prop /default.prop'
+  fi
+  cat $FILES 2>/dev/null | sed -n $REGEX | head -n 1
+}
+
+mv_perm() {
+  mv -f $1 $2 || exit 1
+  set_perm $2 $3 $4 $5 $6
+}
+
+set_perm() {
+  chown $2:$3 $1 || exit 1
+  chmod $4 $1 || exit 1
+  if [ "$5" ]; then
+    chcon $5 $1 2>/dev/null
+  else
+    restorecon $1 2>/dev/null
+  fi
+}
+
+restore_link() {
+  TARGET=$1
+  XPOSED="${1}_xposed"
+  BACKUP="${1}_original"
+  # Don't touch $TARGET if the link was created by something else (e.g. SuperSU)
+  if [ -f $BACKUP -a -L $TARGET -a "$(readlink $TARGET)" = $XPOSED ]; then
+    rm -f $TARGET
+    mv_perm $BACKUP $TARGET $2 $3 $4 $5
+  fi
+  rm -f $XPOSED
+}
+
+restore_backup() {
+  TARGET=$1
+  BACKUP="${1}.orig"
+  NO_ORIG="${1}.no_orig"
+  if [ -f $BACKUP ]; then
+    mv_perm $BACKUP $TARGET $2 $3 $4 $5
+    rm -f $NO_ORIG
+  elif [ -f $NO_ORIG ]; then
+    rm -f $TARGET $NO_ORIG
+  fi
+}
+
+##########################################################################################
+
+echo "********************************"
+echo "Xposed framework uninstaller zip"
+echo "********************************"
+
+echo "- Mounting /system read-write"
+mount /system >/dev/null 2>&1
+mount -o remount,rw /system
+
+echo "- Checking environment"
+API=$(grep_prop ro.build.version.sdk)
+ABI=$(grep_prop ro.product.cpu.abi | cut -c-3)
+ABI2=$(grep_prop ro.product.cpu.abi2 | cut -c-3)
+ABILONG=$(grep_prop ro.product.cpu.abi)
+
+ARCH=arm
+IS64BIT=
+if [ "$ABI" = "x86" ]; then ARCH=x86; fi;
+if [ "$ABI2" = "x86" ]; then ARCH=x86; fi;
+if [ "$API" -ge "21" ]; then
+  if [ "$ABILONG" = "arm64-v8a" ]; then ARCH=arm64; IS64BIT=1; fi;
+  if [ "$ABILONG" = "x86_64" ]; then ARCH=x64; IS64BIT=1; fi;
+else
+  echo "! This script doesn't work for SDK < 21 (yet)"
+  exit 1
+fi
+
+# echo "DBG [$API] [$ABI] [$ABI2] [$ABILONG] [$ARCH]"
+
+echo "- Restoring/removing files"
+rm -f /system/xposed.prop
+rm -f /system/framework/XposedBridge.jar
+
+restore_link   /system/bin/app_process32               0 2000 0755 u:object_r:zygote_exec:s0
+restore_backup /system/bin/dex2oat                     0 2000 0755 u:object_r:dex2oat_exec:s0
+restore_backup /system/bin/oatdump                     0 2000 0755
+restore_backup /system/bin/patchoat                    0 2000 0755 u:object_r:dex2oat_exec:s0
+restore_backup /system/lib/libart.so                   0    0 0644
+restore_backup /system/lib/libart-compiler.so          0    0 0644
+restore_backup /system/lib/libart-disassembler.so      0    0 0644
+restore_backup /system/lib/libsigchain.so              0    0 0644
+restore_backup /system/lib/libxposed_art.so            0    0 0644
+if [ $IS64BIT ]; then
+  restore_link   /system/bin/app_process64             0 2000 0755 u:object_r:zygote_exec:s0
+  restore_backup /system/lib64/libart.so               0    0 0644
+  restore_backup /system/lib64/libart-compiler.so      0    0 0644
+  restore_backup /system/lib64/libart-disassembler.so  0    0 0644
+  restore_backup /system/lib64/libsigchain.so          0    0 0644
+  restore_backup /system/lib64/libxposed_art.so        0    0 0644
+fi
+
+echo "- Done"
+echo
+echo "It's recommended that you wipe the"
+echo "Dalvik cache now."
+
+exit 0
